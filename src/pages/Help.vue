b05f55c7807b8add907a3f964311033ffef58b00
Ludovit Scholtz <ludovit.scholtz@aaaauto.cz>
2020-10-29 20:40:53 +0100

    update help

--- src/pages/Help.vue
+++ src/pages/Help.vue
@@ -10,32 +10,33 @@
       <b-row>
         <b-col>
           <h2 class="text-center">Ako sú moje osobné údaje chránené?</h2>
-          <p>
-            Osobné údaje sú odoslané na server v momente keď stlačíte tlačítko
-            registrácia. Komunikácia so serverom je šifrovaná SSL protokolom.
-          </p>
-          <p>
-            Údaje sú uložené v no-sql databáze. Sú zašifrované AES256 metódou
-            ktorá sa považuje za najvyšší štandard bezpečnosti uložených údajov.
-            2 heslá sú nastavené administrátorom.
-          </p>
-          <p>
-            Autorizovaná osoba môže podľa kódu registrácie načítať vaše osobné
-            údaje. Autorizované osoby sú pozvané do systému. Žiadna z
-            autorizovaných osôb nemá možnosť zobraziť zoznam registračných
-            kódov. Môžu vyhľadávať podľa rodného čísla (v db zahashované), alebo
-            podľa kódu registrácie.
-          </p>
-          <p>
-            Autorizované osoby sa prihlasujú heslom, a autozivoané požiadavky na
-            server prebiehajú s JWT tokenom.
-          </p>
-          <p>
-            Výsledok testovania je dostupný všetkým osobám ktoré majú kód
-            registrácie a posledné 4 čísla rodného čísla. Dbajte na bezpečnosť
-            prideleného kódu registrácie.
-          </p>
-
+          <b-container>
+            <p>
+              Osobné údaje sú odoslané na server v momente keď stlačíte tlačítko
+              registrácia. Komunikácia so serverom je šifrovaná SSL protokolom.
+            </p>
+            <p>
+              Údaje sú uložené v no-sql databáze. Sú zašifrované AES256 metódou
+              ktorá sa považuje za najvyšší štandard bezpečnosti uložených
+              údajov. 2 heslá sú nastavené administrátorom.
+            </p>
+            <p>
+              Autorizovaná osoba môže podľa kódu registrácie načítať vaše osobné
+              údaje. Autorizované osoby sú pozvané do systému. Žiadna z
+              autorizovaných osôb nemá možnosť zobraziť zoznam registračných
+              kódov. Môžu vyhľadávať podľa rodného čísla (v db zahashované),
+              alebo podľa kódu registrácie.
+            </p>
+            <p>
+              Autorizované osoby sa prihlasujú heslom, a autozivoané požiadavky
+              na server prebiehajú s JWT tokenom.
+            </p>
+            <p>
+              Výsledok testovania je dostupný všetkým osobám ktoré majú kód
+              registrácie a posledné 4 čísla rodného čísla. Dbajte na bezpečnosť
+              prideleného kódu registrácie.
+            </p>
+          </b-container>
           <div class="text-center">
             <h2>Registrácia</h2>
             <img
