982bd5d32f82927aa8da4a0c2e6418ba49353247
Ludovit Scholtz <ludovit.scholtz@aaaauto.cz>
2020-11-17 11:01:21 +0100

    user locale sk en

--- src/pages/User.vue
+++ src/pages/User.vue
@@ -4,15 +4,17 @@
       <div class="app-pane-lgray py-2">
         <b-container>
           <h1>
-            {{ $store.state.user.tokenData.Name }} vitajte v systéme pre
-            testovanie covidu
+            {{ $t("userWelcome", { name: $store.state.user.tokenData.Name }) }}
           </h1>
+          <p>
+            {{ $t("userHelp") }}
+          </p>
         </b-container>
       </div>
     </div>
     <div v-else>
       <b-container>
-        <h1>Nemáte oprávnenie k zobrazeniu tejto stránky</h1>
+        <h1>{{ $t("accessDenied") }}</h1>
       </b-container>
     </div>
   </div>
