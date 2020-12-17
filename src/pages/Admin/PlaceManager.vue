bde250735f429c5cae444d27c70a4d23ebbbaab3
Ludovit Scholtz <ludovit.scholtz@aaaauto.cz>
2020-12-17 20:06:52 +0100

    place manager editor

--- src/pages/Admin/PlaceManager.vue
+++ src/pages/Admin/PlaceManager.vue
@@ -29,37 +29,37 @@
             </div>
           </b-tab>
           <b-tab title="Formulár pre správu miesta" active>
-            <h2 v-if="id">Úprava miesta</h2>
+            <h2 v-if="place.id">Úprava miesta</h2>
             <h2 v-else>Nové miesto</h2>
             <b-row>
               <b-col cols="12" md="6">
                 <label for="name">Názov miesta</label>
-                <b-input v-model="name" ref="name" id="name" />
+                <b-input v-model="place.name" ref="name" id="name" />
               </b-col>
               <b-col cols="12" md="6">
                 <label for="address">Adresa</label>
-                <b-input v-model="address" ref="address" id="address" />
+                <b-input v-model="place.address" ref="address" id="address" />
               </b-col>
               <b-col cols="12" md="12">
                 <label for="description">Popis</label>
                 <b-input
-                  v-model="description"
+                  v-model="place.description"
                   ref="description"
                   id="description"
                 />
               </b-col>
               <b-col cols="12" md="3">
                 <label for="lat">GPS Lat</label>
-                <b-input v-model="lat" ref="lat" id="lat" />
+                <b-input v-model="place.lat" ref="lat" id="lat" />
               </b-col>
               <b-col cols="12" md="3">
                 <label for="lng">GPS Lng</label>
-                <b-input v-model="lng" ref="lng" id="lng" />
+                <b-input v-model="place.lng" ref="lng" id="lng" />
               </b-col>
               <b-col cols="12" md="3">
                 <label for="limitPer5MinSlot">Limit / 5min</label>
                 <b-input
-                  v-model="limitPer5MinSlot"
+                  v-model="place.limitPer5MinSlot"
                   ref="limitPer5MinSlot"
                   id="limitPer5MinSlot"
                 />
@@ -67,33 +67,166 @@
               <b-col cols="12" md="3">
                 <label for="limitPer1HourSlot">Limit / hour</label>
                 <b-input
-                  v-model="limitPer1HourSlot"
+                  v-model="place.limitPer1HourSlot"
                   ref="limitPer1HourSlot"
                   id="limitPer1HourSlot"
                 />
               </b-col>
+            </b-row>
+            <b-row class="py-2">
               <b-col cols="12" md="6">
                 <b-form-checkbox
                   id="isDriveIn"
-                  v-model="isDriveIn"
+                  v-model="place.isDriveIn"
                   name="isDriveIn"
                 >
-                  Drive in
+                  Možnosť prísť autom (Drive In)
                 </b-form-checkbox>
               </b-col>
               <b-col cols="12" md="6">
                 <b-form-checkbox
                   id="isWalkIn"
-                  v-model="isWalkIn"
+                  v-model="place.isWalkIn"
                   name="isWalkIn"
                 >
-                  Walk in
+                  Možnosť prísť pešo (Walk in)
                 </b-form-checkbox>
               </b-col>
             </b-row>
+            <b-row>
+              <b-col class="py-2">
+                <h4>
+                  Otváracie hodiny vo formáte
+                  05:55-11:35,11:45-18:00,18:05-20:00
+                </h4>
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col>
+                <label for="openingHoursWorkDay"
+                  >Šablóna otváracích hodín 1 (štandardné)</label
+                >
+                <b-input
+                  v-model="place.openingHoursWorkDay"
+                  ref="openingHoursWorkDay"
+                  id="openingHoursWorkDay"
+                  placeholder="08:00-16:00"
+                />
+              </b-col>
+              <b-col>
+                <label for="openingHoursOther1"
+                  >Šablóna otváracích hodín 2 (víkendy)</label
+                >
+                <b-input
+                  v-model="place.openingHoursOther1"
+                  ref="openingHoursOther1"
+                  id="openingHoursOther1"
+                  placeholder="8:00-12:00, 13:00-17:00"
+                />
+              </b-col>
+              <b-col>
+                <label for="openingHoursOther2"
+                  >Šablóna otváracích hodín 3 (sviatky)</label
+                >
+                <b-input
+                  v-model="place.openingHoursOther2"
+                  ref="openingHoursOther2"
+                  id="openingHoursOther2"
+                  placeholder="13:00-17:00"
+                />
+              </b-col> </b-row
+            ><b-row>
+              <b-col>
+                Správa otváracích dní sa robí na samostatnej stránke
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col class="py-2">
+                <h4>Možnosti testovania</h4>
+              </b-col>
+            </b-row>
+            <b-row class="py-2">
+              <b-col>
+                <b-form-checkbox
+                  id="hasPCRTestFree"
+                  v-model="place.hasPCRTestFree"
+                  name="hasPCRTestFree"
+                >
+                  PCR Test - na predpis
+                </b-form-checkbox>
+              </b-col>
+              <b-col>
+                <b-form-checkbox
+                  id="hasPCRTestSelf"
+                  v-model="place.hasPCRTestSelf"
+                  name="hasPCRTestSelf"
+                >
+                  PCR Test - samoplatca
+                </b-form-checkbox>
+                <b-row v-if="place.hasPCRTestSelf">
+                  <b-col md="8">
+                    <b-input
+                      v-model="place.hasPCRTestSelfPrice"
+                      ref="hasPCRTestSelfPrice"
+                      id="hasPCRTestSelfPrice"
+                      type="number"
+                      step="0.25"
+                      min="0"
+                    />
+                  </b-col>
+                  <b-col md="4">
+                    <b-form-select
+                      :options="currencies"
+                      v-model="place.hasPCRTestSelfPriceCurrency"
+                      ref="hasPCRTestSelfPriceCurrency"
+                      id="hasPCRTestSelfPriceCurrency"
+                    />
+                  </b-col>
+                </b-row>
+              </b-col>
+              <b-col>
+                <b-form-checkbox
+                  id="hasAntTestFree"
+                  v-model="place.hasAntTestFree"
+                  name="hasAntTestFree"
+                >
+                  Antigen Test - zadarmo
+                </b-form-checkbox>
+              </b-col>
+              <b-col>
+                <b-form-checkbox
+                  id="hasAntTestSelf"
+                  v-model="place.hasAntTestSelf"
+                  name="hasAntTestSelf"
+                >
+                  Antigen Test - samoplatca
+                </b-form-checkbox>
+                <b-row v-if="place.hasAntTestSelf">
+                  <b-col md="8">
+                    <b-input
+                      v-model="place.hasAntTestSelfPrice"
+                      ref="hasAntTestSelfPrice"
+                      id="hasAntTestSelfPrice"
+                      type="number"
+                      step="0.25"
+                      min="0"
+                    />
+                  </b-col>
+                  <b-col md="4">
+                    <b-form-select
+                      :options="currencies"
+                      v-model="place.hasAntTestSelfPriceCurrency"
+                      ref="hasAntTestSelfPriceCurrency"
+                      id="hasAntTestSelfPriceCurrency"
+                    />
+                  </b-col>
+                </b-row>
+              </b-col>
+            </b-row>
+
             <b-row
               ><button
-                v-if="id"
+                v-if="place.id"
                 @click="clickCreate"
                 class="govuk-button govuk-!-margin-right-3 govuk-button--start my-4"
               >
@@ -110,7 +243,7 @@
                   <path fill="currentColor" d="M0 0h13l20 20-20 20H0l20-20z" />
                 </svg>
               </button>
-              <a v-if="id" @click="clickCancel" class="btn btn-light">
+              <a v-if="place.id" @click="clickCancel" class="btn btn-light">
                 Zrušiť úpravu
               </a>
               <button
@@ -145,17 +278,31 @@ import { mapActions } from "vuex";
 export default {
   data() {
     return {
+      place: {
+        id: "",
+        name: "",
+        description: "",
+        address: "",
+        lat: 48.289218275462225,
+        lng: 17.272996902465824,
+        isDriveIn: false,
+        isWalkIn: false,
+        limitPer5MinSlot: 5,
+        limitPer1HourSlot: 40,
+        openingHoursWorkDay: "",
+        openingHoursOther1: "",
+        openingHoursOther2: "",
+        hasPCRTestFree: false,
+        hasPCRTestSelf: false,
+        hasPCRTestSelfPrice: 50,
+        hasPCRTestSelfPriceCurrency: "EUR",
+        hasAntTestFree: true,
+        hasAntTestSelf: false,
+        hasAntTestSelfPrice: 5,
+        hasAntTestSelfPriceCurrency: "EUR",
+      },
+      currencies: ["EUR", "CZK", "USD"],
       tabIndex: 0,
-      id: "",
-      name: "",
-      description: "",
-      address: "",
-      lat: 48.289218275462225,
-      lng: 17.272996902465824,
-      isDriveIn: false,
-      isWalkIn: true,
-      limitPer5MinSlot: 5,
-      limitPer1HourSlot: 40,
       fields: [
         {
           label: "Akcia",
@@ -215,16 +362,7 @@ export default {
     },
     clickCreate() {
       this.InsertOrUpdate({
-        id: this.id,
-        name: this.name,
-        description: this.description,
-        address: this.address,
-        lat: this.lat,
-        lng: this.lng,
-        isDriveIn: this.isDriveIn,
-        isWalkIn: this.isWalkIn,
-        limitPer5MinSlot: this.limitPer5MinSlot,
-        limitPer1HourSlot: this.limitPer1HourSlot,
+        id: this.place,
       }).then(r => {
         if (r) {
           this.openSuccess("Uložené");
@@ -237,20 +375,20 @@ export default {
       });
     },
     editPlaceClick(row) {
-      this.id = row.item.id;
-      this.name = row.item.name;
-      this.description = row.item.description;
-      this.address = row.item.address;
-      this.lat = row.item.lat;
-      this.lng = row.item.lng;
-      this.isDriveIn = row.item.isDriveIn;
-      this.isWalkIn = row.item.isWalkIn;
-      this.limitPer5MinSlot = row.item.limitPer5MinSlot;
-      this.limitPer1HourSlot = row.item.limitPer1HourSlot;
+      this.place.id = row.item.id;
+      this.place.name = row.item.name;
+      this.place.description = row.item.description;
+      this.place.address = row.item.address;
+      this.place.lat = row.item.lat;
+      this.place.lng = row.item.lng;
+      this.place.isDriveIn = row.item.isDriveIn;
+      this.place.isWalkIn = row.item.isWalkIn;
+      this.place.limitPer5MinSlot = row.item.limitPer5MinSlot;
+      this.place.limitPer1HourSlot = row.item.limitPer1HourSlot;
       this.tabIndex = 1;
     },
     clickCancel() {
-      this.id = "";
+      this.place.id = "";
       this.tabIndex = 0;
     },
   },
