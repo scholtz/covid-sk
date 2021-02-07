5d9ac1312703c6a9aece2e1915be25b47ddc36cd
Ludovit Scholtz <ludovit.scholtz@aaaauto.cz>
2021-02-07 16:39:37 +0100

    ListAnonymizedVisitors

--- src/pages/DataExporter/Export.vue
+++ src/pages/DataExporter/Export.vue
@@ -115,6 +115,23 @@
             <b-spinner small v-if="loading5" />
           </button>
         </b-col>
+        <b-col>
+          <button
+            @click="clickListAnonymizedVisitors"
+            class="btn btn-primary my-4 form-control"
+          >
+            Export všetkých anonymizovaných
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="17.5"
+              height="19"
+              viewBox="0 0 33 40"
+              role="presentation"
+              focusable="false"
+            ></svg>
+            <b-spinner small v-if="loading6" />
+          </button>
+        </b-col>
       </b-row>
     </b-container>
   </div>
@@ -133,6 +150,7 @@ export default {
       loading3: false,
       loading4: false,
       loading5: false,
+      loading6: false,
     };
   },
   mounted() {
@@ -151,6 +169,7 @@ export default {
       ListAllVisitors: "result/ListAllVisitors",
       ListExportableDays: "result/ListExportableDays",
       ListTestedVisitors: "result/ListTestedVisitors",
+      ListAnonymizedVisitors: "result/ListAnonymizedVisitors",
     }),
     ...mapActions({
       openSuccess: "snackbar/openSuccess",
@@ -208,6 +227,15 @@ export default {
           this.openSuccess("Úspešne ste stiahli súbor");
         }
         this.loading5 = false;
+      });
+    },
+    clickListAnonymizedVisitors() {
+      this.loadin6 = true;
+      this.ListAllVisitors({ day: this.selectedDay }).then(r => {
+        if (r) {
+          this.openSuccess("Úspešne ste stiahli súbor");
+        }
+        this.loadin6 = false;
       });
     },
   },
