403c1074408c5c0cf2bb3169e000deec087917f0
scholtz <ludko2@gmail.com>
2021-02-07 15:58:00 +0000

    Deploying to dist from  @ a8ac0c4389634d804ac4c457495bba121bc392a4 🚀

--- js/chunk-2d0de912.757e093e.js
+++ js/chunk-2d0de912.757e093e.js
@@ -1,2 +1,1 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0de912"],{"85cb":function(n,a,e){"use strict";e.r(a);var t=function(){var n=this,a=n.$createElement,e=n._self._c||a;return e("div",[e("div",{staticClass:"app-pane-lgray py-2"},[e("b-container",[e("h1",[n._v("Správa GDPR súhlasov")])])],1)])},s=[],c={},l=c,p=e("2877"),r=Object(p["a"])(l,t,s,!1,null,null,null);a["default"]=r.exports}}]);
-//# sourceMappingURL=chunk-2d0de912.757e093e.js.map
