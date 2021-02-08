0f8f2e19d79e358b63616070d7efdf29d5d1951c
samuel@zubo.sk <jahoda76>
2021-02-08 13:19:21 +0100

    obchodne podmienky

--- src/pages/PlaceProvider/Terms.vue
+++ src/pages/PlaceProvider/Terms.vue
@@ -9,18 +9,20 @@
       <b-row
         ><b-col md="12">
           <p>
-            Prevádzkovateľ služby je <b>Scholtz &amp; Company, j.s.a.</b> so
+            Dodávateľ služby je <b>Scholtz &amp; Company, j.s.a.</b> so
             sídlom Švabinského 21, 851 01 Bratislava. IČO spoločnosti: 51 882
-            272, IČ DPH: SK2120828105.
+            272, IČ DPH: SK2120828105 (ďalej aj Dodávateľ).
           </p>
           <p>
-            Prevádzkovateľ poskytuje <b>službu</b> predregistrácie návštevníka
-            pre odber vzorky na testovacie miesto proti chorobe covid-19, IT
-            služby pre odberové miesto pre scanovanie a kontrolu registrácie a
-            zadávanie výsledkov testov.
+            Dodávateľ prevádzkuje <b>online službu Rýchlejšie.sk</b> (ďalej ako Rýchlejšie.sk) 
+            predregistrácie návštevníka na realizáciu <b>služby</b> poskytovanej <b>prevádkovateľom</b> služby. 
+            Prevádzkovateľom služby sa rozumie mesto, zamestnávateľ, MOM, inštitúcia a pod..
+            Dodávateľ zabezpečuje online IT služby pre odberové miesto pre 
+            skenovanie a kontrolu registrácie, zadávanie výsledkov testov, reporting,
+            poskytovanie výsledkov testov vo forme SMS a elektronického certifikátu.
           </p>
           <p>
-            Prevádzkovateľ určil osobu zodpovednú za spracovanie osobných údajov
+            Dodávateľ určil osobu zodpovednú za spracovanie osobných údajov
             v rámci zákona o <b>GDPR</b>: Ľudovít Scholtz, kontakt:
             scholtzandcojsa@gmail.com, Telefón +420 776 082 012. Dozorujúci
             orgán: Úrad na ochranu osobných údajov Slovenskej republiky,
@@ -28,68 +30,55 @@
             32313214, zodpovednaosoba@pdp.gov.sk, statny.dozor@pdp.gov.sk
           </p>
           <p>
-            Prevádzkovateľ zabezpečí <b>šifrovanie osobných údajov</b>, a
+            Dodávateľ zabezpečí <b>šifrovanie osobných údajov</b>, a
             ďalších citlivých údajov ako je napríklad výsledok testovania.
             Zabezpečí zamedzenie prístupnosti k osobným údajom v minimálnom
             rozsahu pre personál testovacieho alebo vakcinačného miesta vrátane
             kontaktných údajov.
           </p>
           <p>
-            Systém je webové rozhranie a backendové služby zriadené
-            dodávateľom pre poskytovanie služieb.
+            Služba Rýchlejšie.sk sa skladá z webového rozhrania a backendových služieb zriadených
+            dodávateľom.
           </p>
           <p>
             Osobné údaje sú uložené v cloude v Microsoft Azure, v lokalite West Europe.
           </p>
           <p>
-            <b>Prevádzkovateľom odberového alebo vakcinačného miesta</b> sa rozumie osoba
+            <b>Prevádzkovateľom služby odberového alebo vakcinačného miesta</b> sa rozumie osoba
             oprávnená na prevádzku odberového alebo vakcinačného miesta na odber
             vzoriek proti chorobe Covid-19. Iba oprávnená osoba (konateľ,
             predseda predstavenstva..) môže zaregistrovať správcu odberového
-            alebo vakcinačného miesta do systému.
+            alebo vakcinačného miesta do systému ako Prevádzkovateľa.
           </p>
           <p>
-            Registráciou správcovi nevzniká žiadna povinnosť. Všetky poskytnuté
-            služby dodávateľom sú poskytnuté na základe objednávky alebo
-            jeho nastavení v systéme (napr. výkon platených služieb).
+            Registráciou prevádzkovateľovi nevzniká žiadna povinnosť. Všetky poskytnuté
+            služby dodávateľom sú poskytnuté na základe objednávky a to aj vo forme e-mailu, alebo
+            prostredníctvom nastavení v systéme (napr. výkon platených služieb).
           </p>
           <p>
-            Objednávka obsahuje úroveň SLA, a počet registrácií. Cenník je
-            dostupný na adrese
+            Objednávka obsahuje očakávaný objem vybavených klientov (napr. otestovaných osôb). 
+            Cenník je dostupný na adrese
             <a
-              href="https://www.rychlejsie.sk#/pricing"
+              href="https://www.rychlejsie.sk/#pricing"
               target="_blank"
               class="btn bg-light text-dark p-1"
-              >https://www.rychlejsie.sk#/pricing</a
-            >. SLA - SHARED znamená zdieľaná infraštruktúra, kde sa za hosting
-            neplatí. Negarantuje však žiadnu dostupnosť. SLA - BRONZE garantuje
-            dostupnosť 600 requestov za minútu s dostupnosťou 90% času v
-            oberných hodinách. SLA - SILVER garantuje dostupnosť 100 000
-            requestov za minútu s dostupnosťou 95% času v oberných hodinách. SLA
-            - GOLD garantuje dostupnosť 5 000 000 requestov za minútu s
-            dostupnosťou 99% času v oberných hodinách. Requestom sa rozumie
-            každé HTTP pripojenie štandardne z prehliadača na webový server.
-            Balík komunikácie K1 obsahuje 1000 registrovaných návštevníkov.
-            Balík komunikácie K15 obsahuje 15000 registrovaných návštevníkov.
-            Balík komunikácie K100 obsahuje 100000 registrovaných návštevníkov.
-            SLA sa vzťahuje na dni používania. Za každý deň sa platí stanovený
-            poplatok. Je možné upgradnuť SLA na vyšší level a poskytovateľ môže,
-            ale nemusí poskytnúť v takom prípade zľavu za službu.
+              >https://www.rychlejsie.sk/#pricing</a
+            >.
           </p>
           <p>
             Dodávateľ poskytne prevádzkovateľovi odberového miesta službu až
             <b>po úhrade</b> objednávky, alebo zálohy.
           </p>
           <p>
-            Prevádzkovateľ odberového alebo vakcinačného miesta môže poskytovať platené
-            službu. Prevádzkovateľ vystaví faktúru každej osobe ktorá sa
-            zaregistruje na test v cene určenou správcom miesta plus servisný
-            poplatok 5% plus DPH. Prevádzkovateľ má nárok na servisný poplatok
+            Prevádzkovateľ odberového alebo vakcinačného miesta môže prostredníctvom služby Rýchlejšie.sk 
+            poskytovať aj platené služby. Dodávateľ vystaví faktúru každej osobe ktorá sa
+            zaregistruje na test v cene určenej prevádzkovateľom služby plus servisný
+            poplatok 5% plus DPH. Dodávateľ má nárok na servisný poplatok
             5% z obratu. Prevádzkovateľ odberového alebo vakcinačného miesta má nárok na
             100% z jeho určenej hodnoty poplatkov z každej zaplatenej faktúry.
             Prevádzkovateľ odberového alebo vakcinačného miesta vystaví
             dodávateľovi faktúru medzi 1. až 7. dňom nasledujúceho mesiaca
-            s hodnoutou všetkých transakcií v predchádzajúcom mesiaci so
+            s hodnotou všetkých transakcií v predchádzajúcom mesiaci so
             splatnosťou 14 dní. Prevádzkovateľ je povinný faktúru uhradiť.
           </p>
           <p>
@@ -97,13 +86,12 @@
             súd pri Komoditnej burze Bratislava.
           </p>
           <p>
-            Aplikovateľné právo je právo Slovenskej Republiky, obchodný
-            zákonník.
+            Aplikovateľné právo je právo Slovenskej Republiky, obchodný zákonník.
           </p>
           <p>
-            Prevádzkovateľ si vyhradzuje právo na zmenu obchodných podmienok.
+            Dodávateľ si vyhradzuje právo na zmenu obchodných podmienok.
           </p>
-          <p>Tieto obchodné podmienky sú platné a účinné od 15.1.2021.</p>
+          <p>Tieto obchodné podmienky sú platné a účinné od 8.2.2021.</p>
         </b-col>
       </b-row>
     </b-container>
